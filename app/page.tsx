9b073de2cdf5befc5f19084c5c91d1c68ec2b67e
theBigBengi <imthebigbengi@gmail.com>
2024-02-08 13:41:42 +0200

    just for now

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
       <div className='hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0' />
       <div className='my-16 text-center animate-fade-in'>
         <h2 className='text-sm text-zinc-500 '>
-          I'm building{" "}
+          Im building{" "}
           <Link
             target='_blank'
             href='https://unkey.dev'
